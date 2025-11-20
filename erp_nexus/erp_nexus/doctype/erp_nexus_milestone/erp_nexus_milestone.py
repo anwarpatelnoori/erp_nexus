2227b22c3ede23029fd500fff954218f6882e095
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2025-11-20 12:03:04 +0400

    fix: ERP Nexus Milestone

---
+++ erp_nexus/erp_nexus/doctype/erp_nexus_milestone/erp_nexus_milestone.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, CubeZixOne and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ERPNexusMilestone(Document):
+	pass
