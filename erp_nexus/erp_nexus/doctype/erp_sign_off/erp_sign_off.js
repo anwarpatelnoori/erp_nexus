2689a57979b0c26f330d4572eedce5cfee6f220d
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2025-11-20 12:07:02 +0400

    fix: all doc in custom app

---
+++ erp_nexus/erp_nexus/doctype/erp_sign_off/erp_sign_off.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, CubeZixOne and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ERP Sign Off", {
+// 	refresh(frm) {
+
+// 	},
+// });
